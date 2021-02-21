9ce45d131b97cace6a9568bd69b79033cec085e7
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2021-02-21 11:33:55 +0100

    Basic layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: "RJ-coding blog",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ],
 };
---
+++ src/layouts/default.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+const Aside = () => {
+    return (
+        <>
+            <h1>RJ's Development Blog</h1>
+            <sub>
+                <p>My name is Arjan, or RJ, and I am an engineer that turned software developer.</p>
+                <p>I always like to learn and experiment and to experience new ways of doing the exactly same things.</p>
+                <p>This blog is a place for me to write down my thoughts and experiences and hopefully start a discussion in order to learn from each other.</p>
+            </sub>
+            <hr />
+        </>
+    )
+}
+
+const DefaultLayout = ({ aside, main }) => {
+    aside = aside || <Aside />
+
+    return (
+        <div className="layout default">
+            <aside>{ aside }</aside>
+            <main><div className="content">{ main }</div></main>
+        </div>
+    )
+}
+
+export default DefaultLayout
