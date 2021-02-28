34d20bc4e1083f2e5d8538e528d3568b1db3e619
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2021-02-28 07:32:45 +0100

    Typo

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -6,7 +6,7 @@ const DefaultLayout = ({ children }) => {
         <>
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-lg d-flex">
-                    <span className="navbar-brand mt-2 mb-2 h1"><a className="text-decoration-none text-white" href="/">@avwie programming blog</a></span>
+                    <span className="navbar-brand mt-2 mb-2 h1"><a className="text-decoration-none text-white" href="/">@avwie's programming blog</a></span>
                     <div className="text-light mx-3">
                         <a title="Github" href="https://github.com/avwie" target="_blank" rel="noopener">
                             <img alt="Github" width="24" height="24" src="/layout/github.png" />
