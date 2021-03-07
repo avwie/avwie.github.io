a390fb4a54bcbd6c5a1e29a21e73552b5810b4fb
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2021-03-07 07:42:16 +0100

    Added Google Analytics

--- src/html.js
+++ src/html.js
@@ -29,6 +29,14 @@ export default function HTML(props) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
             crossOrigin="anonymous"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZ9NCTW594" />
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YZ9NCTW594');
+        </script>
       </body>
     </html>
   )
